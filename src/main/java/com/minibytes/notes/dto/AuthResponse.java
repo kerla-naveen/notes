bcef14f0ea1de983eb97b3134035a74c5b2b4576
naveen-kerla <kerlanaveen19@gmail.com>
2026-04-21 14:49:34 +0530

    feat: implement login, registration dto's

---
+++ src/main/java/com/minibytes/notes/dto/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.minibytes.notes.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+}
